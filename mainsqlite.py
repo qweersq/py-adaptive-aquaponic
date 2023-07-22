48998cda442a227cc9ff62e8fc9721c1385a3aba
Hadad Al Akbar <75150256+qweersq@users.noreply.github.com>
2023-07-22 19:42:55 +0700

    new sqlite

--- mainsqlite.py
+++ mainsqlite.py
@@ -21,6 +21,7 @@ cursor.execute(
 )"""
 )
 
+
 def insert_sample_data():
     current_time = datetime.now()
     cursor.execute(
@@ -30,9 +31,10 @@ def insert_sample_data():
     conn.commit()
     print("Sampel data berhasil dimasukkan ke local database")
 
+
 def check_internet_connection():
     try:
-        print("Check Koneksi ke server ...")
+        print("=> Cek Koneksi ke server ...")
         response = requests.get("https://aquaponic.sinamlab.com/api/pool")
         if response.status_code == 200:
             return True
@@ -52,15 +54,15 @@ def send_data_to_server():
     )
     data = cursor.fetchall()
     # print(data)
-    
+
     # check data if length is 5
     if len(data) < 5:
-        print("Data kurang dari 5, tidak mengirim data ke server")
+        print("=> Data kurang dari 5, tidak mengirim data ke server\n")
     else:
         # Kirim data ke server VPS (contoh)
         for row in data:
             # Kode untuk mengirim data ke server VPS
-            print(f"Data yang dikirim ke server : {row}")
+            print(f"=> Send Data : {row}")
             payload = {
                 "pool_id": row[1],
                 "temper_val": row[2],
@@ -79,19 +81,38 @@ def send_data_to_server():
                     """UPDATE sensor_data SET is_sent = 1 WHERE id = ?""", (row[0],)
                 )
                 conn.commit()
-                print("Data berhasil dikirim ke server")
+                print("=> Data berhasil dikirim ke server\n")
             else:
-                print("Data gagal dikirim ke server")
+                print("=> Data gagal dikirim ke server\n")
 
 
-def save_data_to_local(pool_id, temper_val, ph_val, humidity_val, oxygen_val, tds_val, turbidities_val, is_sent):
+def save_data_to_local(
+    pool_id,
+    temper_val,
+    ph_val,
+    humidity_val,
+    oxygen_val,
+    tds_val,
+    turbidities_val,
+    is_sent,
+):
     current_time = datetime.now()
     cursor.execute(
         """INSERT INTO sensor_data (pool_id, temper_val, ph_val, humidity_val, oxygen_val, tds_val, turbidities_val, is_sent, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-        (pool_id, temper_val, ph_val, humidity_val, oxygen_val, tds_val, turbidities_val, is_sent, current_time),
+        (
+            pool_id,
+            temper_val,
+            ph_val,
+            humidity_val,
+            oxygen_val,
+            tds_val,
+            turbidities_val,
+            is_sent,
+            current_time,
+        ),
     )
     conn.commit()
-    print("Data tersimpan di local database")
+    print("=> Data tersimpan di local database\n")
 
 
 def delete_old_data():
@@ -100,47 +121,55 @@ def delete_old_data():
         (datetime.now() - timedelta(days=30),),
     )
     conn.commit()
-    print("Menhapus data lama berhasil")
+    print("=> Menghapus data satu bulan lalu berhasil\n")
+
 
 def main():
-    data = {
-        "pool_id" : 2,
-        "temper_val" : 25.39,
-        "ph_val" : 7.2,
-        "humidity_val" : 40.0,
-        "oxygen_val" : 4.2,
-        "tds_val" : 9.17,
-        "turbidities_val" : 1988.11,
-        "is_sent" : 0,
-    }
-    
-    save_data_to_local(
-        data["pool_id"],
-        data["temper_val"],
-        data["ph_val"],
-        data["humidity_val"],
-        data["oxygen_val"],
-        data["tds_val"],
-        data["turbidities_val"],
-        data["is_sent"],
-    )
-    
-    if check_internet_connection():
-        print("Terhubung ke server")
-        send_data_to_server()
-    else:
-        print("Tidak terhubung ke server")
-        
-    delete_old_data()
-    get_all_data()
+    while True:
+        # program dibawah akan dijalankan ketika satu menit sekali
+        now = datetime.now()
+        if now.second == 0 and now.microsecond == 0:
+            data = {
+                "pool_id": 2,
+                "temper_val": 25.39,
+                "ph_val": 7.2,
+                "humidity_val": 40.0,
+                "oxygen_val": 4.2,
+                "tds_val": 1693,
+                "turbidities_val": 1988.11,
+                "is_sent": 0,
+            }
+
+            save_data_to_local(
+                data["pool_id"],
+                data["temper_val"],
+                data["ph_val"],
+                data["humidity_val"],
+                data["oxygen_val"],
+                data["tds_val"],
+                data["turbidities_val"],
+                data["is_sent"],
+            )
+
+            if check_internet_connection():
+                print("=> Terhubung ke server\n")
+                send_data_to_server()
+            else:
+                print("=> Tidak terhubung ke server")
+                print("=> Data telah tersimpan di local database\n")
+
+            delete_old_data()
+            get_all_data()
+
 
 def get_all_data():
     cursor.execute("""SELECT * FROM sensor_data""")
     data = cursor.fetchall()
-    print("All data")
+    print("\nSemua Data di local database")
     for row in data:
         print(row)
-        
+
+
 def get_data_certain_time():
     cursor.execute(
         """SELECT * FROM sensor_data WHERE created_at >= ? AND is_sent = 0""",
@@ -150,13 +179,15 @@ def get_data_certain_time():
     print("5 minutes ago")
     for row in data:
         print(row)
-        
+
+
 def get_data_from_server():
     url = "https://aquaponic.sinamlab.com/api/pooldata"
     headers = {"Content-type": "application/json"}
-    
+
     response = requests.get(url, headers=headers)
     print(response.json())
+
 
 main()
 # check_internet_connection()
@@ -166,4 +197,3 @@ main()
 # delete_old_data()
 # get_all_data()
 # get_data_from_server()
-
